4d5a8794c9b4e88fd209a014e2098c6c846c9109
ilyasmalki72009-source <ilyasmalki72009@gmail.com>
2026-08-25 12:59:58 +0100

    Update and rename ai.js to test.js

--- api/test.js
+++ api/test.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   return res.status(200).json({
     success: true,
-    message: "AI API IS WORKING",
+    message: "TEST API WORKS",
     method: req.method
   });
 }
